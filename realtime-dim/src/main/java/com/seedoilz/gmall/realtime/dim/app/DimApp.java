d29b46dde18306ff9960c341590b57bcf08eab4e
seedoilz <seedoilz609@qq.com>
2024-05-03 19:23:06 +0800

    Flink CDC test

--- realtime-dim/src/main/java/com/seedoilz/gmall/realtime/dim/app/DimApp.java
+++ realtime-dim/src/main/java/com/seedoilz/gmall/realtime/dim/app/DimApp.java
@@ -1,12 +1,13 @@
 package com.seedoilz.gmall.realtime.dim.app;
 
 import com.seedoilz.gmall.realtime.common.base.BaseApp;
+import com.seedoilz.gmall.realtime.common.constant.Constant;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class DimApp extends BaseApp {
     public static void main(String[] args) {
-        new DimApp().start(10001,4, "dim_app", "topic_db");
+        new DimApp().start(10001,4, "dim_app", Constant.TOPIC_DB);
     }
 
     @Override
